722ad2fbe93619f9eb834dafa80ea43a8c49e9bf
HieuNT_HE130901 <147804799+HE130901@users.noreply.github.com>
2024-07-16 10:48:46 +0700

    setiing

--- cms-server/cms-server/Models/CmsContext.cs
+++ cms-server/cms-server/Models/CmsContext.cs
@@ -49,7 +49,7 @@ public partial class CmsContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:sep490g65.database.windows.net,1433;Initial Catalog=cms;Persist Security Info=False;User ID=trunghieu;Password=Concho@2024;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        => optionsBuilder.UseSqlServer("Server = trunghieu; Initial Catalog = cms; Persist Security Info=False;User ID = sa; Password=abcd1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout = 30;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
