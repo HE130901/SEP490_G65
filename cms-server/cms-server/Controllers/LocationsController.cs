8d53887988e901b2acbdd523d467de965035fad9
huyvthe130028 <huyvthe130028@fpt.edu.vn>
2024-08-05 21:43:40 +0700

    add LocationsController

--- cms-server/cms-server/Controllers/LocationsController.cs
+++ cms-server/cms-server/Controllers/LocationsController.cs
@@ -1,3 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using cms_server.Models;
 using cms_server.Services;
+
+namespace cms_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        private readonly ILocationService _locationService;
+
+        public LocationsController(ILocationService locationService)
+        {
+            _locationService = locationService;
+        }
