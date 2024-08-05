a32d78cf2ef8fa742ada5cd188c96bcac5280aa5
ptkanh <anhptkhe130580@fpt.edu.vn>
2024-08-05 22:49:57 +0700

    Create ContractForStaffController.cs

---
+++ cms-server/cms-server/Controllers/ContractForStaffController.cs
@@ -0,0 +1,30 @@
+using cms_server.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using cms_server.DTOs;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using MimeKit.Text;
+using MimeKit;
+using MailKit.Net.Smtp;
+using cms_server.Configuration;
+
+namespace cms_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContractForStaffController : ControllerBase
+    {
+        private readonly CmsContext _context;
+
+        private readonly IConfiguration _configuration;
+
+        public ContractForStaffController(CmsContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+
+    }
+}
