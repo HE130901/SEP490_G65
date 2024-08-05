70dcab79182fe477e1829596dd9dd16ce0d64602
huyvthe130028 <huyvthe130028@fpt.edu.vn>
2024-08-05 21:39:55 +0700

    add CustomerDto

--- cms-server/cms-server/Controllers/CustomersController.cs
+++ cms-server/cms-server/Controllers/CustomersController.cs
@@ -177,3 +177,13 @@ public class ChangePasswordDto2
     {
         public string Password { get; set; }
     }
+public class CustomerDto
+    {
+        public int CustomerId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string CitizenId { get; set; }
+    }
+}
