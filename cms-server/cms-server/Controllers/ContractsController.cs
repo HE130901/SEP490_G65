0ccc450e2eab36047fcaa57e0c851734009aff02
ptkanh <anhptkhe130580@fpt.edu.vn>
2024-08-05 22:37:46 +0700

    Update ContractsController.cs
    add getContractByCustomer

--- cms-server/cms-server/Controllers/ContractsController.cs
+++ cms-server/cms-server/Controllers/ContractsController.cs
@@ -23,6 +23,44 @@ namespace cms_server.Controllers
             _context = context;
         }
 
+	// GET: api/{customerId}/list
+        [HttpGet("{customerId}/list")]
+        public async Task<ActionResult<IEnumerable<ContractDto>>> GetContractsByCustomer(int customerId)
+        {
+            // Get contracts for the specified customer
+            var contracts = await _context.Contracts
+                .Include(c => c.Customer)
+                .Include(c => c.Deceased)
+                .Include(c => c.Niche)
+                    .ThenInclude(n => n.Area)
+                        .ThenInclude(a => a.Floor)
+                            .ThenInclude(f => f.Building)
+                .Where(c => c.CustomerId == customerId)
+                .Select(c => new ContractDto
+                {
+                    NicheId = c.NicheId,
+                    ContractId = c.ContractId,
+                    ContractCode = c.ContractCode,
+                    CustomerName = c.Customer.FullName,
+                    DeceasedName = c.Deceased != null ? c.Deceased.FullName : "Không có thông tin",
+                    DeceasedRelationshipWithCustomer = c.Deceased != null ? c.Deceased.RelationshipWithCusomer : "Không có thông tin",
+                    StartDate = c.StartDate,
+                    EndDate = c.EndDate,
+                    Status = c.Status,
+                    NicheName = $"{c.Niche.Area.Floor.Building.BuildingName} - {c.Niche.Area.Floor.FloorName} - {c.Niche.Area.AreaName} - Ô {c.Niche.NicheName}",
+                    DaysLeft = (c.Status == "Expired" || c.Status == "Canceled" || !c.EndDate.HasValue)
+                        ? 0
+                        : Math.Max((c.EndDate.Value.ToDateTime(TimeOnly.MinValue) - DateTime.Now).Days, 0)
+                })
+                .ToListAsync();
+
+            if (contracts == null || !contracts.Any())
+            {
+                return NotFound();
+            }
+
+            return contracts;
+        }
 
 
     }
