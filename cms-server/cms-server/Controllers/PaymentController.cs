b2d55b47a3b55a41ee703619258cac72070c69c2
inin15 <101159760+inin15@users.noreply.github.com>
2024-08-05 22:27:53 +0700

    update create payment

--- cms-server/cms-server/Controllers/PaymentController.cs
+++ cms-server/cms-server/Controllers/PaymentController.cs
@@ -29,3 +29,11 @@ namespace cms_server.Controllers
             string orderId = model.OrderId;
             string createDate = DateTime.Now.ToString("yyyyMMddHHmmss");
             string expireDate = DateTime.Now.AddDays(1).ToString("yyyyMMddHHmmss");
+var vnpay = new VnPayLibrary();
+            vnpay.AddRequestData("vnp_Version", "2.1.0");
+            vnpay.AddRequestData("vnp_Command", "pay");
+            vnpay.AddRequestData("vnp_TmnCode", _tmnCode);
+            vnpay.AddRequestData("vnp_Amount", amount);
+            vnpay.AddRequestData("vnp_CurrCode", "VND");
+            vnpay.AddRequestData("vnp_TxnRef", orderId);
+            vnpay.AddRequestData("vnp_OrderInfo", "Thanh toan don hang " + orderId);
