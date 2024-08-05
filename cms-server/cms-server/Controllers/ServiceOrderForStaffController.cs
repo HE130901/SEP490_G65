a9b2ab32a11ed03fc1c69c6a4247757f47f62342
inin15 <101159760+inin15@users.noreply.github.com>
2024-08-05 22:45:40 +0700

    create service orderforstaff

--- cms-server/cms-server/Controllers/ServiceOrderForStaffController.cs
+++ cms-server/cms-server/Controllers/ServiceOrderForStaffController.cs
@@ -20,3 +20,27 @@ namespace cms_server.Controllers
             _context = context;
           //  _notificationService = notificationService;
         }
+private async Task<decimal> CalculateServiceOrderTotalAsync(int serviceOrderId)
+        {
+            var totalPrice = await _context.ServiceOrderDetails
+                .Where(sod => sod.ServiceOrderId == serviceOrderId)
+                .Join(_context.Services, sod => sod.ServiceId, s => s.ServiceId,
+                      (sod, s) => new { sod.Quantity, s.Price })
+                .SumAsync(x => (decimal?)(x.Quantity * x.Price)) ?? 0;
+
+            return totalPrice;
+        }
+
+        private int GetStaffIdFromToken()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            if (identity != null)
+            {
+                var staffIdClaim = identity.FindFirst(ClaimTypes.NameIdentifier);
+                if (staffIdClaim != null)
+                {
+                    return int.Parse(staffIdClaim.Value);
+                }
+            }
+            throw new UnauthorizedAccessException("Invalid token");
+        }
