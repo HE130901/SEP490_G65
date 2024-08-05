c01be7f356b74af013c987e5d4ae3e9b7ef6512e
ptkanh <anhptkhe130580@fpt.edu.vn>
2024-08-05 21:56:42 +0700

    create authcontroller, endpoint login

---
+++ cms-server/cms-server/Controllers/AuthController.cs
@@ -0,0 +1,87 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using cms_server.DTOs;
+using System.Text;
+using MimeKit;
+using MailKit.Net.Smtp;
+using MimeKit.Text;
+using Microsoft.Extensions.Configuration;
+using Castle.Core.Resource;
+using cms_server.Configuration;
+
+namespace cms_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly CmsContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(CmsContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+
+        // POST: /api/auth/register
+        [HttpPost("login")]
+        public IActionResult Login(LoginDto loginDto)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Email == loginDto.Email);
+
+            // Check if the user exists and the password is correct
+            if (customer != null && BCrypt.Net.BCrypt.Verify(loginDto.Password, customer.PasswordHash))
+            {
+                var token = GenerateJwtToken(customer.CustomerId.ToString(), "Customer", customer.Phone, customer.Address);
+                return Ok(new
+                {
+                    Token = token,
+                    Role = "Customer"
+                });
+            }
+
+            var staff = _context.Staff.SingleOrDefault(s => s.Email == loginDto.Email);
+
+            // Check if the user exists and the password is correct
+            if (staff != null && BCrypt.Net.BCrypt.Verify(loginDto.Password, staff.PasswordHash))
+            {
+                var token = GenerateJwtToken(staff.StaffId.ToString(), staff.Role, staff.Phone, staff.Email);
+                return Ok(new
+                {
+                    Token = token,
+                    Id = staff.StaffId,
+                    Name = staff.FullName,
+                    Email = staff.Email,
+                    Role = staff.Role
+                });
+            }
+
+            return Unauthorized("Invalid credentials.");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
