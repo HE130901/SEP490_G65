4f17a8234fdf04b82178d141f256827c4f745def
inin15 <101159760+inin15@users.noreply.github.com>
2024-08-05 22:44:25 +0700

    create service orderfor staff

---
+++ cms-server/cms-server/Controllers/ServiceOrderForStaffController.cs
@@ -0,0 +1,16 @@
+using cms_server.Configuration;
+using cms_server.Models;
+using cms_server.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace cms_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceOrderForStaffController : ControllerBase
+    {
+        private readonly CmsContext _context;
+        //private readonly INotificationService _notificationService;
