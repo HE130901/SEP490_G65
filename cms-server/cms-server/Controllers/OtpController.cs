c029ee031467b802a461740c1245add606703a46
inin15 <101159760+inin15@users.noreply.github.com>
2024-08-05 22:22:35 +0700

    update create otp

--- cms-server/cms-server/Controllers/OtpController.cs
+++ cms-server/cms-server/Controllers/OtpController.cs
@@ -83,3 +83,9 @@ public class SendOtpRequest
 {
     public string PhoneNumber { get; set; }
 }
+public class VerifyOtpRequest
+{
+    public string PhoneNumber { get; set; }
+    public string Otp { get; set; }
+}
+
