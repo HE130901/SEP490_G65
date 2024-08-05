a14f667d94ae81ef34b3c14ce85c23f526719031
ptkanh <anhptkhe130580@fpt.edu.vn>
2024-08-05 22:51:55 +0700

    Update ContractForStaffController.cs
    add GenerateRandomPassword

--- cms-server/cms-server/Controllers/ContractForStaffController.cs
+++ cms-server/cms-server/Controllers/ContractForStaffController.cs
@@ -29,5 +29,14 @@ namespace cms_server.Controllers
         {
             return await _context.Deceaseds.AnyAsync(d => d.DeathCertificateNumber == deathCertificateNumber);
         }
+
+        private string GenerateRandomPassword(int length = 12)
+        {
+            const string validChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?_-";
+            var random = new Random();
+            return new string(Enumerable.Repeat(validChars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
     }
 }
