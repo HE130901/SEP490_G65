9ff8a54ec01a9d39fc8f8a118010363c423a0e3a
ptkanh <anhptkhe130580@fpt.edu.vn>
2024-08-05 22:52:28 +0700

    Update ContractForStaffController.cs
    add CreateContract

--- cms-server/cms-server/Controllers/ContractForStaffController.cs
+++ cms-server/cms-server/Controllers/ContractForStaffController.cs
@@ -38,5 +38,54 @@ namespace cms_server.Controllers
                 .Select(s => s[random.Next(s.Length)]).ToArray());
         }
 
+        // POST: api/ContractForStaff/create-contract
+        [HttpPost("create-contract")]
+        [Authorize]
+        public async Task<IActionResult> CreateContract(CreateContractRequest request)
+        {
+            // Lấy StaffId từ token
+            var staffIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (staffIdClaim == null || !int.TryParse(staffIdClaim, out int staffId))
+            {
+                return Unauthorized("Invalid or missing Staff ID in token.");
+            }
+
+            using (var transaction = await _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    // Kiểm tra và lấy thông tin niche
+                    var niche = await _context.Niches.FirstOrDefaultAsync(n => n.NicheId == request.NicheID &&
+                                 (n.Status == "Available" || n.Status == "Booked"));
+
+                    if (niche == null)
+                    {
+                        return BadRequest("Ô chứa không khả dụng để đặt chỗ");
+                    }
+
+                    // Kiểm tra và lấy thông tin khách hàng
+                    var customer = await _context.Customers
+                        .FirstOrDefaultAsync(c => c.CitizenId == request.CustomerCitizenId);
+
+                    if (customer == null)
+                    {
+                        // Tạo khách hàng mới nếu không tồn tại
+                        customer = new Customer
+                        {
+                            FullName = request.CustomerFullName,
+                            Phone = request.CustomerPhoneNumber,
+                            Email = request.CustomerEmail,
+                            Address = request.CustomerAddress,
+                            CitizenId = request.CustomerCitizenId,
+                            CitizenIdissuanceDate = request.CustomerCitizenIdIssueDate,
+                            CitizenIdsupplier = request.CustomerCitizenIdSupplier,
+                            // Mật khẩu mặc định abcdabcd
+                            PasswordHash = "$2a$11$nUOFWiAMFi4zIAbIkYAbcuhFx3JYvT4ELKpBE6kh7IN5S9/wsfk4q"
+                            //PasswordHash = BCrypt.Net.BCrypt.HashPassword(GenerateRandomPassword())
+                    };
+                        _context.Customers.Add(customer);
+                        await _context.SaveChangesAsync();
+
+
     }
 }
