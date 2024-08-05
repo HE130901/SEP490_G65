0287042748a637d054ef6d36c2ad9304fa8d5a78
huyvthe130028 <huyvthe130028@fpt.edu.vn>
2024-08-05 21:31:33 +0700

    Thêm mới endpoint xem danh sách ô chứa

---
+++ cms-server/cms-server/Controllers/NichesController.cs
@@ -0,0 +1,36 @@
+using cms_server.DTOs;
+using cms_server.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace cms_server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    
+    public class NichesController : ControllerBase
+    {
+        private readonly INicheService _nicheService;
+
+        public NichesController(INicheService nicheService)
+        {
+            _nicheService = nicheService;
+        }
+       
+        [HttpGet("customer")]
+        [Authorize]
+        public async Task<ActionResult<IEnumerable<NicheDto>>> GetNiches()
+        {
+            var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+            if (userIdClaim == null)
+            {
+                return Unauthorized("User ID not found in token");
+            }
+            int customerId = int.Parse(userIdClaim.Value);
+
+            var niches = await _nicheService.GetNichesAsync(customerId);
+            return Ok(niches);
+        }
