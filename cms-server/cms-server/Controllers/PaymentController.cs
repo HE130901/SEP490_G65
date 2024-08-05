fb44ab883eb0f92427c46d1fcf030cc495d2bf73
inin15 <101159760+inin15@users.noreply.github.com>
2024-08-05 22:33:11 +0700

    create payment

--- cms-server/cms-server/Controllers/PaymentController.cs
+++ cms-server/cms-server/Controllers/PaymentController.cs
@@ -106,3 +106,8 @@ public class PaymentRequestModel
         public string Amount { get; set; }
         public string OrderId { get; set; }
     }
+ public class PaymentResponseModel
+    {
+        public string PaymentUrl { get; set; }
+    }
+}
