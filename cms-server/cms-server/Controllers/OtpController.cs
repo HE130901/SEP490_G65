24b1b538c083c1760e30f5f36da99aed5404e93f
inin15 <101159760+inin15@users.noreply.github.com>
2024-08-05 22:17:21 +0700

    Create Otp service

---
+++ cms-server/cms-server/Controllers/OtpController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OtpController : ControllerBase
+{
+    private readonly IOtpService _otpService;
+
+    public OtpController(IOtpService otpService)
+    {
+        _otpService = otpService;
+    }
