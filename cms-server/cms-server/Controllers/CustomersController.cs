2db82dc0ee85c4e646b8152f59e98a25c6c35bf3
huyvthe130028 <huyvthe130028@fpt.edu.vn>
2024-08-05 21:39:07 +0700

    add PostCustomer

--- cms-server/cms-server/Controllers/CustomersController.cs
+++ cms-server/cms-server/Controllers/CustomersController.cs
@@ -129,3 +129,35 @@ namespace cms_server.Controllers
 
             return NoContent();
         }
+
+// POST: api/Customers
+        [HttpPost]
+        public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCustomer", new { id = customer.CustomerId }, customer);
+        }
+
+        // DELETE: api/Customers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.Customers.Any(e => e.CustomerId == id);
+        }
+    }
