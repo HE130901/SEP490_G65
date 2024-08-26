f53b71d66f2105271d5ae462aabab416211ff19e
HE130901 <147804799+HE130901@users.noreply.github.com>
2024-08-26 21:07:57 +0700

    Update otpVerificationService.ts

--- cms-customer/cms-customer/src/services/otpVerificationService.ts
+++ cms-customer/cms-customer/src/services/otpVerificationService.ts
@@ -4,7 +4,7 @@ import axiosInstance from "@/utils/axiosInstance";
 const API_URL = '/api/Otp';
 
 // Real OTP functions
-/*
+
 const sendOtp = async (phoneNumber: any) => {
   const response = await axiosInstance.post(`${API_URL}/send-otp`, { phoneNumber });
   return response.data;
@@ -14,18 +14,18 @@ const verifyOtp = async (phoneNumber: any, otp: any) => {
   const response = await axiosInstance.post(`${API_URL}/verify-otp`, { phoneNumber, otp });
   return response.data;
 };
-*/
+
 
 // Fake OTP functions for testing
-const sendOtp = async (phoneNumber: any) => {
-  const response = await axiosInstance.post(`${API_URL}/test/send-otp`, { phoneNumber });
-  return response.data;
-};
+// const sendOtp = async (phoneNumber: any) => {
+//   const response = await axiosInstance.post(`${API_URL}/test/send-otp`, { phoneNumber });
+//   return response.data;
+// };
 
-const verifyOtp = async (phoneNumber: any, otp: any) => {
-  const response = await axiosInstance.post(`${API_URL}/test/verify-otp`, { phoneNumber, otp });
-  return response.data;
-};
+// const verifyOtp = async (phoneNumber: any, otp: any) => {
+//   const response = await axiosInstance.post(`${API_URL}/test/verify-otp`, { phoneNumber, otp });
+//   return response.data;
+// };
 
 const OTPVerificationAPI = {
   sendOtp,
